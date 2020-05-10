de408aae489cac8ffc9daed099505f7407c10492
mrboggii <60938259+mrboggii@users.noreply.github.com>
2020-05-10 02:27:25 +0200

    imgs0

--- daou-app/src/App.js
+++ daou-app/src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Saluda = props => {
+  console.log(props)
+  return <h2>Hola {props.name} </h2>
+};
+
 function App() {
+  const hola = <h2>Hola mundo React </h2>
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+                  <Saluda name="boggii" age={23} />
+                  <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
